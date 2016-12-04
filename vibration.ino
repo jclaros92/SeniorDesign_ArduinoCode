c7f394bb7dbf4e9154d3fc672c988c81ea33c21e
jclaros92 <jclaros92@gmail.com>
2016-12-03 22:38:31 -0500

    Add files via upload

---
+++ vibration.ino
@@ -0,0 +1,29 @@
+void setup() {
+  pinMode( 6 , OUTPUT);  // Must be a PWM pin
+}
+
+void loop() {
+  int x;
+  int y = 0;
+
+ analogWrite( 6 , 153 );  // 60% duty cycle
+  delay(10000);              // on time
+
+  analogWrite( 6 , 0 );    // 0% duty cycle (off)
+  delay(1000);             // off time
+
+
+
+ 
+  
+  for( x = 0 ; x < 154 ; x++ ) 
+  {
+    analogWrite( 6 , x );
+    delay(100);
+  }
+  
+  analogWrite( 6 , 0 ); //off
+
+  
+
+}
